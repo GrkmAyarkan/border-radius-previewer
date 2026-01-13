877fb496e661a160c2ee8c006494cc3b615ae505
GrkmAyarkan <grkm.ayarkan@gmail.com>
2026-01-13 18:31:30 +0300

    Add inputs and Js setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const previewBox = document.querySelector('.preview-box');
+
+const topLeftInput = document.getElementById('top-left');
+const topRightInput = document.getElementById('top-right');
+const bottomRightInput = document.getElementById('bottom-right');
+const bottomLeftInput = document.getElementById('bottom-left');
